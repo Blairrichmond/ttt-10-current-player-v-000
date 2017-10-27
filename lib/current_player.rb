b0f70967bd9c302bb2b32f48f0a94960c2cdf181
Brent  Ransom <sadumedusa@gmail.com>
2017-10-27 19:17:45 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,7 @@
 def turn_count(board)
   counter = 0
   board.each do |mark|
+    counter += 1
   if mark = 3
     return 3
   end
@@ -10,7 +11,7 @@ end
 def current_player(board)
   if turn_count(board) % 2
     return "X"
-  else 
+  else
     return "O"
   end
 end
